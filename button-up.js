1a74809c4d6e928854f571570b8afb35996e9ac6
ValentinaLuna04 <valentinaluna416@gmail.com>
2024-05-28 20:27:57 -0300

    Sass-5

--- button-up.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("scroll", () => {
-    const UpButton  = document.querySelector("#up");
-
-    if (window.scrollY > 200) {
-        UpButton.classList.add("show")
-    } else {
-        UpButton.classList.remove("show")
-    }
-})
